97ce5001e8785d0c3fec8b620caccafca395a77e
trinebra <157688079+trinebra@users.noreply.github.com>
2024-01-28 18:21:18 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function colorchange (){
-    document.getElementById("farger").style.color="pink" ;
+function colorchange(){
+    document.getElementById('farger').style.color ="darkblue";
+}
 
+function background(){
+    document.getElementById('background').style.backgroundColor =('gray');
 }
